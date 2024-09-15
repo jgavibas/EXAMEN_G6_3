d62b6b2a96a67b2cb516edb5f906f7874392053f
Julio Avila <julio.avibas@gmail.com>
2024-09-14 23:26:26 -0500

    SUBIDA INICIAL

---
+++ EX_G6_3/src/main/java/org/examen/g6/ex3/controller/UsersController.java
@@ -0,0 +1,21 @@
+package org.examen.g6.ex3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@Tag(
+        name = "API GESTION DE USUARIOS",
+        description = "Esta api contiene los endPoints para la gestión de USUARIOS, " +
+                "desde crear hasta eliminar de manera logica")
+public class UsersController {
+
+}
